ea81efe2bd58bc3c73a8f8e4ce7e8d58796ce37f
Alif Andarta Al Falah <alifandarta26@gmail.com>
2022-12-25 11:13:12 +0700

    Initial commit project

---
+++ src/main/java/mii/mcc72/ams_server_app/AmsServerAppApplication.java
@@ -0,0 +1,13 @@
+package mii.mcc72.ams_server_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsServerAppApplication.class, args);
+	}
+
+}
