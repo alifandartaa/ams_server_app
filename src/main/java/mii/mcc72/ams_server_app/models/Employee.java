479bc7a804bae37d15009fd30858efe5e384f9a2
Alif Andarta Al Falah <alifandarta26@gmail.com>
2022-12-25 15:24:30 +0700

    add employee entity

---
+++ src/main/java/mii/mcc72/ams_server_app/models/Employee.java
@@ -0,0 +1,32 @@
+package mii.mcc72.ams_server_app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Department department;
+}
