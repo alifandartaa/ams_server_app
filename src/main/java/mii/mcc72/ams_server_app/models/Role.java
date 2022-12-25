01eee9f156f79bc715a7e596b63332bbd869f04d
Alif Andarta Al Falah <alifandarta26@gmail.com>
2022-12-25 12:50:42 +0700

    add role entity

---
+++ src/main/java/mii/mcc72/ams_server_app/models/Role.java
@@ -0,0 +1,29 @@
+package mii.mcc72.ams_server_app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "role_privilege",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "privilege_id")
+    )
+    private List<Privilege> privileges;
+}
