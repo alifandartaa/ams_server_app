182676be2980ae5c30b1b1e757e004f8952bb24b
Alif Andarta Al Falah <alifandarta26@gmail.com>
2022-12-25 19:14:13 +0700

    add report entity

--- src/main/java/mii/mcc72/ams_server_app/repos/ReportRepo.java
+++ src/main/java/mii/mcc72/ams_server_app/repos/ReportRepo.java
@@ -1,9 +1,9 @@
 package mii.mcc72.ams_server_app.repos;
 
-import mii.mcc72.ams_server_app.models.Role;
+import mii.mcc72.ams_server_app.models.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepo extends JpaRepository<Role, Integer> {
+public interface ReportRepo extends JpaRepository<Report, Integer> {
 }
